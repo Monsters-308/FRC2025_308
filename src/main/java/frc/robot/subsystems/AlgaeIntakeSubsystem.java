2ccaceca640d7d0be5c667846af0c7d35a008216
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-01-18 10:08:02 -0500

    intake conf

---
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstaints;
+
+/**
+ * Subsystem that controls the Ground intake.
+ */
+public class AlgaeIntakeSubsystem extends SubsystemBase {
+    private final SparkMax m_algaeIntakeMotorRight = new SparkMax(IntakeConstaints.kIntakeMotorId, MotorType.kBrushless);
+    private final SparkMax m_algaeIntakeMotorLeft = new SparkMax(IntakeConstaints.kIntakeMotorId, MotorType.kBrushless);
+
+
+    public AlgaeIntakeSubsystem() {
+        SparkMaxConfig leftMotorConf = new SparkMaxConfig();
+        SparkMaxConfig rightMotorConf = new SparkMaxConfig();
+
+
+
+        // sets the configation of the motors
+        leftMotorConf
+            .inverted(false)
+            .idleMode(null)
+            .smartCurrentLimit(1);
+        rightMotorConf
+            .inverted(false)
+            .idleMode(null)
+            .smartCurrentLimit(0)
+            .follow(m_algaeIntakeMotorLeft);
+
+        
+    }
+    
+}
