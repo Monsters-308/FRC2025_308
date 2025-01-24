4db86f959a8cfd899a28301561f9dcb42331760b
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-01-23 19:27:19 -0500

    AAAAAAAAAAAAAAAAAAAAAAH

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import java.security.AlgorithmConstraints;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
@@ -13,27 +15,32 @@ import frc.robot.Constants.AlgaeIntakeConstaints;
  * Subsystem that controls the Ground intake.
  */
 public class AlgaeIntakeSubsystem extends SubsystemBase {
-    private final SparkMax m_algaeIntakeMotorRight = new SparkMax(AlgaeIntakeConstaints.kIntakeMotorId, MotorType.kBrushless);
+    private final SparkMax m_algaeIntakeMotorArm = new SparkMax(AlgaeIntakeConstaints.kIntakeMotorId, MotorType.kBrushless);
     private final SparkMax m_algaeIntakeMotorLeft = new SparkMax(AlgaeIntakeConstaints.kIntakeMotorId, MotorType.kBrushless);
 
 
     public AlgaeIntakeSubsystem() {
-        SparkMaxConfig leftMotorConf = new SparkMaxConfig();
-        SparkMaxConfig rightMotorConf = new SparkMaxConfig();
+        SparkMaxConfig RollerMotorConf = new SparkMaxConfig();
+        SparkMaxConfig ArmMotorConf = new SparkMaxConfig();
 
 
 
         // sets the configation of the motors
-        leftMotorConf
+        RollerMotorConf
             .inverted(AlgaeIntakeConstaints.kleftAlgaeIntakeInverted)
             .idleMode(null)
             .smartCurrentLimit(1);
-        rightMotorConf
+        ArmMotorConf
             .inverted(AlgaeIntakeConstaints.krightAlgaeIntakeInverted)
             .idleMode(null)
             .smartCurrentLimit(0);
+
+            m_algaeIntakeMotorLeft.configure(RollerMotorConf, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+            m_algaeIntakeMotorArm.configure(ArmMotorConf, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
-    
+    public void periodic(){
+
+    }
 
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,10 +1,36 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerFeedbackType;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase{
     private final SparkMax m_armMotor = new SparkMax(0, null);
-    
+
+
+    SparkMaxConfig armMotorConf = new SparkMaxConfig();
+
+    public ArmSubsystem(){
+        armMotorConf
+        .inverted(false)
+        .smartCurrentLimit(0)
+        .idleMode(null);
+
+        
+
+        m_armMotor.configure(armMotorConf, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public void periodic(){
+        
+    }
+
 }
+    
+
