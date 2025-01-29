d26f09746627fe19b7a58d192ffb36e0797c67c3
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-01-28 19:12:15 -0500

    fix the photon vision

--- src/main/java/frc/robot/subsystems/PhotonSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonSubsystem.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems;
 import java.util.Optional;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import edu.wpi.first.math.estimator.PoseEstimator3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PhotonConstants;
 
@@ -27,7 +24,7 @@ public class PhotonSubsystem extends SubsystemBase {
 
     public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
         photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-        return photonPoseEstimator.update();
+        return photonPoseEstimator.update(camera.getLatestResult());
     }
 
 }
