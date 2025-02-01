edfe1d01f062465f4f491603d0b2c265191b14d1
tg1523342 <tg1523342@icloud.com>
2025-02-01 09:12:03 -0500

    Added input mappongs class

---
+++ src/main/java/frc/utils/InputMappings.java
@@ -0,0 +1,12 @@
+package frc.utils;
+
+// import org.json.*;
+
+/**
+ * Loads an input mapping of the controller throgh a JSON file.
+ */
+public class InputMappings {
+    private InputMappings() throws Exception {
+        throw new Exception("InputMappings should never be instantiated.");
+    }
+}
