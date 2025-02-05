2d744ee6fccad2564108f8fe83e1ed1ca7f50cb7
tg1523342 <tg1523342@icloud.com>
2025-02-05 18:21:57 -0500

    Added alliance relative

--- src/main/java/frc/robot/commands/drive/RobotFacePoint.java
+++ src/main/java/frc/robot/commands/drive/RobotFacePoint.java
@@ -36,16 +36,19 @@ public class RobotFacePoint extends Command {
 
     private final Translation2d m_point;
 
+    private final boolean m_allianceRelative;
+
     /**
      * This command rotates the robot in space using the pose estimation compared to a given point on the field.
      * The driver still has full control over the X and Y of the robot.
      */
-    public RobotFacePoint(VisionSubsystem visionSubsystem, DriveSubsystem driveSubsystem, DoubleSupplier xSpeed, DoubleSupplier ySpeed, Translation2d point){
+    public RobotFacePoint(VisionSubsystem visionSubsystem, DriveSubsystem driveSubsystem, DoubleSupplier xSpeed, DoubleSupplier ySpeed, Translation2d point, boolean allianceRelative){
         m_driveSubsystem = driveSubsystem;
         m_visionSubsystem = visionSubsystem;
         m_xSpeed = xSpeed;
         m_ySpeed = ySpeed;
         m_point = point;
+        m_allianceRelative = allianceRelative;
 
         angleController.enableContinuousInput(-180, 180);
         angleController.setTolerance(HeadingConstants.kHeadingTolerance);
@@ -62,7 +65,7 @@ public class RobotFacePoint extends Command {
      */
     //When not overridden, this function is blank.
     @Override
-    public void initialize(){
+    public void initialize() {
         m_visionSubsystem.setPipeline(VisionConstants.kAprilTagPipeline);
         angleController.reset();
         m_complete = false;
@@ -72,10 +75,10 @@ public class RobotFacePoint extends Command {
      * Once you want the function to end, you should set m_complete to true.
      */
     @Override
-    public void execute(){
+    public void execute() {
         
         Translation2d pos1 = m_driveSubsystem.getPose().getTranslation(); // Position of robot on field
-        Translation2d pos2 = FieldUtils.flipRed(m_point); // 2D point on field (adjusted for alliance) 
+        Translation2d pos2 = m_allianceRelative ? FieldUtils.flipRed(m_point) : m_point; // 2D point on field (adjusted for alliance) 
         Rotation2d angleToTarget = OdometryUtils.anglePoseToPose(pos1, pos2); // Angle to make robot face point
 
         // Set pid controller to angle to make robot face point
@@ -97,7 +100,7 @@ public class RobotFacePoint extends Command {
     }
 
     @Override
-    public boolean isFinished(){
+    public boolean isFinished() {
         return m_complete;
     }
 }
