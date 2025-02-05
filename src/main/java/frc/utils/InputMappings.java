5daa2761a2f0020ec34110e164399e0397421c33
tg1523342 <tg1523342@icloud.com>
2025-02-05 09:14:33 -0500

    Added a way to use s sendabke chooser to select mappings

--- src/main/java/frc/utils/InputMappings.java
+++ src/main/java/frc/utils/InputMappings.java
@@ -1,5 +1,6 @@
 package frc.utils;
 
+import java.io.File;
 import java.io.FileReader;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -16,36 +18,105 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * Loads an input mapping of the controller throgh a JSON file.
  */
 public class InputMappings {
-    private static JSONParser parser = new JSONParser();
+    private static JSONParser m_parser = new JSONParser();
     
-    private static Map<String, CommandXboxController> controllers = new HashMap<>();
+    private static Map<String, CommandXboxController> m_controllers = new HashMap<>();
+    private static Map<String, SendableChooser<String>> m_choosers = new HashMap<>();
 
     private InputMappings() throws Exception {
         throw new Exception("InputMappings should never be instantiated.");
     }
 
     public static void registerController(String id, CommandXboxController controller) {
-        controllers.put(id, controller);
+        m_controllers.put(id, controller);
     }
 
-    public static Trigger getTrigger(String controllerId, String mapId, String triggerId) {
+    public static Trigger getTrigger(String controllerId, String triggerId) {
         try {
-            JSONObject obj = (JSONObject)parser.parse(new FileReader(Filesystem.getDeployDirectory().getPath() + "/mappings/" + controllerId + "/" + mapId + ".json"));
-            JSONObject triggers = (JSONObject)obj.get("triggers");
-            JSONObject triggerData = (JSONObject)triggers.get(triggerId);
-            String triggerType = (String)triggerData.get("button");
-            double threshold = (double)triggerData.get("threshold");
-            CommandXboxController controller = controllers.get(controllerId);
+            CommandXboxController controller = m_controllers.get(controllerId);
 
-            Method triggerMethod = controller.getClass().getDeclaredMethod(triggerType);
+            File mappingsDirectory = Filesystem.getDeployDirectory().toPath().resolve(
+                "mappings" + File.separatorChar + controllerId
+            ).toFile();
 
-            if (triggerType.endsWith("trigger")) {
-                return (Trigger)triggerMethod.invoke(controller, threshold); 
+            File[] mappings = mappingsDirectory.listFiles();
+            Trigger[] triggers = new Trigger[mappings.length];
+
+            for (int i = 0; i < mappings.length; i++) {
+                JSONObject obj = (JSONObject)m_parser.parse(new FileReader(mappings[i]));
+                JSONObject mappingTriggers = (JSONObject)obj.get("triggers");
+                JSONObject triggerData = (JSONObject)mappingTriggers.get(triggerId);
+                String triggerType = (String)triggerData.get("button");
+
+                Double threshold = (Double)triggerData.get("threshold");
+
+                if (triggerType.endsWith("Trigger") && threshold != null) {
+                    Method triggerMethod = controller.getClass().getDeclaredMethod(triggerType, double.class);
+
+                    triggers[i] = (Trigger)triggerMethod.invoke(controller, threshold);
+                    continue;
+                }
+
+                Method triggerMethod = controller.getClass().getDeclaredMethod(triggerType);
+                triggers[i] = (Trigger)triggerMethod.invoke(controller);
             }
 
-            return (Trigger)triggerMethod.invoke(controller);
+            Trigger combinedTrigger = triggers[0].and(
+                () -> getChooser(controllerId).getSelected() == mappings[0].getAbsolutePath()
+            );
+
+            for (int i = 1; i < triggers.length; i++) {
+                final int index = i; // so "i" can be used inside a lambda
+
+                combinedTrigger = combinedTrigger.or(
+                    triggers[i].and(
+                        () -> getChooser(controllerId).getSelected() == mappings[index].getAbsolutePath()
+                    )
+                );
+            }
+
+            return combinedTrigger;
         } catch(Exception e) {
             return null;
         }
+    }
+
+    public static SendableChooser<String> getChooser(String controllerId) {
+        SendableChooser<String> chooser = m_choosers.get(controllerId);
+
+        if (chooser == null) {
+            try {
+                chooser = new SendableChooser<>();
+
+                File mappingsDirectory = Filesystem.getDeployDirectory().toPath().resolve(
+                    "mappings" + File.separatorChar + controllerId
+                ).toFile();
+
+                File[] mappings = mappingsDirectory.listFiles();
+
+                for (File mapping : mappings) {
+                    String displayName;
+
+                    displayName = (String)((JSONObject)m_parser.parse(new FileReader(mapping))).get("displayName");
+
+                    if (displayName == null) {
+                        displayName = mapping.getName().replaceAll("\\.json", "");
+                    }
+
+                    if (mapping.getName() == "default.json") {
+                        chooser.setDefaultOption(displayName, mapping.getAbsolutePath());
+                        continue;
+                    }
+
+                    chooser.addOption(displayName, mapping.getAbsolutePath());
+                }
+
+                m_choosers.put(controllerId, chooser);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        return chooser;
     }
 }
