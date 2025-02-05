7e7f68c99c2938492032b8b441205a83eb926808
tg1523342 <tg1523342@icloud.com>
2025-02-05 18:21:08 -0500

    Added auto align positions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -117,6 +118,9 @@ public final class Constants {
         public static final int KFrontRightTurningEncoderId = 12;
         public static final int KRearLeftTurningEncoderId = 13;
         public static final int KRearRightTurningEncoderId = 11;
+
+        // Auto align
+        public static final Pose2d[] kAutoAlignPositions = {};
     }
 
     /**
