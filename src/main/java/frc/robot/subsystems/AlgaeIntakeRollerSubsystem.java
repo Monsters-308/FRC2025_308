eda1c05b5e5909ae91d8d9580733579a37a12a83
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-02-15 09:53:10 -0500

    Update AlgaeIntakeRollerSubsystem.java

--- src/main/java/frc/robot/subsystems/AlgaeIntakeRollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeRollerSubsystem.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.RelativeEncoder;
+
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
