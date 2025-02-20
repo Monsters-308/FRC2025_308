7ab10cdcda2b8a8effbf8e8cdbd0c20c32ed9c32
tg1523342 <tg1523342@icloud.com>
2025-02-20 13:06:18 -0500

    Moved utils into robot folder

--- src/main/java/frc/robot/utils/InputMappings.java
+++ src/main/java/frc/robot/utils/InputMappings.java
@@ -1,4 +1,4 @@
-package frc.utils;
+package frc.robot.utils;
 
 import java.io.File;
 import java.io.FileReader;
--- src/main/java/frc/robot/utils/LoggingUtils.java
+++ src/main/java/frc/robot/utils/LoggingUtils.java
@@ -1,4 +1,4 @@
-package frc.utils;
+package frc.robot.utils;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
