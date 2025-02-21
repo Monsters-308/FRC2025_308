197f277dac91ec0e33f41a7c1984680122260c7a
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-02-20 20:14:18 -0500

    q it is q

--- src/main/java/frc/robot/commands/auton/GoToLevel.java
+++ src/main/java/frc/robot/commands/auton/GoToLevel.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.auton;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
@@ -8,11 +8,12 @@ import frc.robot.subsystems.ElevatorSubsystem;
 /*
  * This command will set the elevator to the 4th level.
  */
-public class GoToLevel extends ParallelCommandGroup {
+public class GoToLevel extends SequentialCommandGroup {
     
     public GoToLevel(ArmSubsystem armSubsystem, ElevatorSubsystem elevatorSubsystem, int index) {
 
         super(armSubsystem.goToLevel(index, false), elevatorSubsystem.goToLevel(index, false));
 
     }
+
 }
